51b8269fcb4cac096603cb2b12b594c00b8fe346
Kyle Fang <zhigang1992@gmail.com>
2013-02-26 10:59:37 +0800

    Initial Commit

---
+++ ZGPullDragTableViewTests/ZGPullDragTableViewTests.h
@@ -0,0 +1,13 @@
+//
+//  ZGPullDragTableViewTests.h
+//  ZGPullDragTableViewTests
+//
+//  Created by Kyle Fang on 2/26/13.
+//  Copyright (c) 2013 Kyle Fang. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ZGPullDragTableViewTests : SenTestCase
+
+@end
---
+++ ZGPullDragTableViewTests/ZGPullDragTableViewTests.m
@@ -0,0 +1,32 @@
+//
+//  ZGPullDragTableViewTests.m
+//  ZGPullDragTableViewTests
+//
+//  Created by Kyle Fang on 2/26/13.
+//  Copyright (c) 2013 Kyle Fang. All rights reserved.
+//
+
+#import "ZGPullDragTableViewTests.h"
+
+@implementation ZGPullDragTableViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ZGPullDragTableViewTests");
+}
+
+@end
